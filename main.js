1907c0ffd40541b37a0219c5c30853ee233fc26b
fankysnow <fankysnow@gmail.com>
2025-07-18 06:15:10 +0300

    update

--- main.js
+++ main.js
@@ -4,32 +4,40 @@ const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 const searchInputElement = document.querySelector('#input-city');
 const searchBtnElement = document.querySelector('#btn-search');
 const weatherIconElement = document.querySelector('#weather-icon');
+const weatherCardElement  = document.querySelector('.weather');
+const errorElement = document.querySelector('.error');
+const cityNameElement = document.querySelector('.city');
+const temperatureTextElement = document.querySelector('.temp');
+const humidityTextElement = document.querySelector('.humidity');
+const windyTextElement = document.querySelector('.wind');
 
 const checkWeather = async (city) => {
     try {
         const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
-        let data = await response.json();
 
         if(response.status === 404 || searchInputElement.value.length === 0) {
-            document.querySelector('.error').style.display = 'block';
-            document.querySelector('.weather').style.display = 'none';
+            errorElement.style.display = 'block';
+            weatherCardElement.style.display = 'none';
         }
 
-        document.querySelector('.city').innerText = searchInputElement.value[0].toUpperCase() + searchInputElement.value.slice(1);
-        document.querySelector('.temp').innerText = Math.round(data.main.temp) + '°C';
-        document.querySelector('.humidity').innerText = data.main.humidity + '%';
-        document.querySelector('.wind').innerText = data.wind.speed + 'km/h';
+        let data = await response.json();
 
-        document.querySelector('.weather').style.display = 'block';
-        document.querySelector('.error').style.display = 'none';
+        cityNameElement.innerText = searchInputElement.value[0].toUpperCase() + searchInputElement.value.slice(1);
+        temperatureTextElement.innerText = Math.round(data.main.temp) + '°C';
+        humidityTextElement.innerText = data.main.humidity + '%';
+        windyTextElement.innerText = data.wind.speed + 'km/h';
 
-        switch (data.weather[0].main) {
-            case 'Clouds': weatherIconElement.src="images/clouds.webp"; break;
-            case 'Clear': weatherIconElement.src="images/clear.webp"; break;
-            case 'Rain': weatherIconElement.src="images/rain.webp"; break;
-            case 'Drizzle': weatherIconElement.src="images/drizzle.webp"; break;
-            case 'Mist': weatherIconElement.src="images/mist.webp"; break;
-        }
+        weatherCardElement.style.display = 'block';
+        errorElement.style.display = 'none';
+
+        const weatherIcons = {
+            Clouds: "images/clouds.webp",
+            Clear: "images/clear.webp",
+            Rain: "images/rain.webp",
+            Drizzle: "images/drizzle.webp",
+            Mist: "images/mist.webp",
+        };
+        weatherIconElement.src = weatherIcons[data.weather[0].main];
     }
     catch (err) {
         console.log("Ошибка, неверно заполненное поле ввода мур.");
