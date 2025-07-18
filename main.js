5c36f695907021dd4923879408b6d10de12d7d89
fankysnow <fankysnow@gmail.com>
2025-07-18 05:33:45 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const apiKey = 'e5114048d532f83801c0f3bb6e77048a';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=';
 
-const searchInput = document.querySelector('.search input');
-const searchBtn = document.querySelector('.search button');
-const weatherIcon = document.querySelector('.weather-icon');
+const searchInputElement = document.querySelector('.search input');
+const searchBtnElement = document.querySelector('.search button');
+const weatherIconElement = document.querySelector('.weather-icon');
 
 const checkWeather = async (city) => {
     const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
@@ -14,7 +14,7 @@ const checkWeather = async (city) => {
         document.querySelector('.weather').style.display = 'none';
     }
 
-    document.querySelector('.city').innerText = searchInput.value[0].toUpperCase() + searchInput.value.slice(1);
+    document.querySelector('.city').innerText = searchInputElement.value[0].toUpperCase() + searchInputElement.value.slice(1);
     document.querySelector('.temp').innerText = Math.round(data.main.temp) + '°C';
     document.querySelector('.humidity').innerText = data.main.humidity + '%';
     document.querySelector('.wind').innerText = data.wind.speed + 'km/h';
@@ -23,18 +23,18 @@ const checkWeather = async (city) => {
     document.querySelector('.error').style.display = 'none';
 
     switch (data.weather[0].main) {
-        case 'Clouds': weatherIcon.src="images/clouds.webp"; break;
-        case 'Clear': weatherIcon.src="images/clear.webp"; break;
-        case 'Rain': weatherIcon.src="images/rain.webp"; break;
-        case 'Drizzle': weatherIcon.src="images/drizzle.webp"; break;
-        case 'Mist': weatherIcon.src="images/mist.webp"; break;
+        case 'Clouds': weatherIconElement.src="images/clouds.webp"; break;
+        case 'Clear': weatherIconElement.src="images/clear.webp"; break;
+        case 'Rain': weatherIconElement.src="images/rain.webp"; break;
+        case 'Drizzle': weatherIconElement.src="images/drizzle.webp"; break;
+        case 'Mist': weatherIconElement.src="images/mist.webp"; break;
     }
 }
 
-searchBtn.addEventListener('click', () => {
-        checkWeather(searchInput.value.trim());
+searchBtnElement.addEventListener('click', () => {
+        checkWeather(searchInputElement.value.trim());
 });
 
-searchInput.addEventListener( 'keyup', event => {
-    if(event.code === 'Enter') checkWeather(searchInput.value.trim());
+searchInputElement.addEventListener( 'keyup', event => {
+    if(event.code === 'Enter') checkWeather(searchInputElement.value.trim());
 });
